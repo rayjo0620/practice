9b1990dfefee53107ed948e5f90eb4662c0008da
rayjo0620 <rayjo0620@gmail.com>
2020-12-29 11:26:38 +0900

    2020.12.29 rayjo0620 최초 커밋

---
+++ practiceTest/src/main/java/com/example/demo/PracticeTestApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeTestApplication.class, args);
+		
+		System.out.println("Hello world");
+	}
+
+}
